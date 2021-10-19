d3fa3762ed1ac8390f2b51420c66d075fe4deeae
JDS111 <42532309+JDS111@users.noreply.github.com>
2021-10-19 15:21:06 +0500

    commit 1

---
+++ components/CakeIsALieGame.py
@@ -0,0 +1,14 @@
+import random
+rand_num = random.randint(0, 1000)
+game_status = True
+def cakeisalie():
+   print("привет это игра угадай число");
+while game_status:
+   a = int( input("взломай пароль: ") )
+   if a == rand_num:
+      print("хакер!")
+      game_status = False
+   elif a > rand_num:
+      print("попробуй меньше!")  
+   else:
+      print("попробуй больше!")
