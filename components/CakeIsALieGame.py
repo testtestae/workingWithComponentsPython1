dfafa84d470c4d09e58dc0d79db1fb6b8848057f
торт ложь <92367852+CakeIsAlie1@users.noreply.github.com>
2021-10-19 15:49:58 +0500

    Update CakeIsALieGame.py

--- components/CakeIsALieGame.py
+++ components/CakeIsALieGame.py
@@ -2,7 +2,7 @@ import random
 rand_num = random.randint(0, 1000)
 game_status = True
 def cakeisalie():
-   print("привет это игра угадай число");
+   print("привет это игра взломай пароль");
 cakeisalie()
 while game_status:
    a = int( input("взломай пароль: ") )
